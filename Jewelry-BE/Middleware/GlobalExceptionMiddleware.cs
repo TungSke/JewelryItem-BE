aa389acf7f85668fdf89bbc528e628eff516d30d
TrinhSonTung <trinhsontung2410@gmail.com>
2024-07-29 20:30:28 +0700

    update middleware print detail message

--- Jewelry-BE/Middleware/GlobalExceptionMiddleware.cs
+++ Jewelry-BE/Middleware/GlobalExceptionMiddleware.cs
@@ -20,16 +20,21 @@
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Unexpected exception");
+
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = 500;
 
-                await context.Response.WriteAsync(new
+                var response = new
                 {
                     Code = 500,
-                    Message = ex.Message
-                }.ToString() ?? string.Empty);
+                    Message = ex.Message,
+                    Detailed = ex.StackTrace
+                };
+
+                var responseText = System.Text.Json.JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(responseText);
             }
         }
-
     }
 }
